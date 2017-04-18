fde5134dcb392a2a8fd8b49fefc9f9721627c356
gregwchase <gregory.chase@colorado.edu>
2017-04-18 11:54:33 -0600

    Added data subsets

--- sf_Salaries.py
+++ sf_Salaries.py
@@ -1,40 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 salaries = pd.read_csv("/Users/gregorychase/Desktop/sf-salaries/Salaries.csv")
 
-
-dims = salaries.shape
-
-# Replace values marked "Not Provided"
-# salaries = salaries.replace("Not Provided", np.NaN)
-# salaries = salaries.replace(np.NaN,0)
-
-# TotalComp already given as TotalPay, TotalPayBenefits
-# salaries["TotalComp"] = salaries["BasePay"] + salaries["OvertimePay"] + salaries["OtherPay"]
-
-
-# salaries.BasePay = salaries.BasePay.astype(np.int64)
-
-
-# salaries = salaries.fillna({
-#     'JobTitle': 'missing',
-#     'BasePay': '0',
-#     'OvertimePay': '0',
-#     'OtherPay': '0',
-#     'Benefits': '0',
-#     'Year': '0',
-#     'Status': '0'
-# })
-
 salaries.head()
-
-
-salaries.dtypes
+# salaries.dtypes
 
 
 # Convert object values to numeric
-# df = df.convert_objects(convert_numeric=True)
 # 'coerce' converts invalid values to NaN, 'ignore' returns original Series
 salaries['BasePay'] = pd.to_numeric(salaries['BasePay'], errors='coerce')
 salaries['OvertimePay'] = pd.to_numeric(salaries['OvertimePay'], errors='coerce')
@@ -48,32 +22,26 @@ salaries.drop('Agency', axis=1, inplace=True)
 # salaries.Notes.value_counts()
 salaries.drop('Notes', axis=1, inplace=True)
 
-# Fill all missing values/ NaN with 0
-# df.fillna(0)
-
-
-# Calculations of mean default to exclude NaN values
-# Read in data
-# Keep NaN data
-# Convert columns to numeric
-# Begin asking visualization questions
-
 salaries.BasePay.mean()
 salaries.OvertimePay.mean()
 salaries.OtherPay.mean()
+salaries.TotalPay.mean()
 
 salaries.BasePay.median()
 salaries.OvertimePay.median()
 salaries.OtherPay.median()
-
+salaries.TotalPay.median()
 
 # Percent of data listed as full time (FT) or part time (PT)
-round(sum(salaries.Status.value_counts())/float(len(salaries)) * 100,2)
+pct_FT_or_PT = round(sum(salaries.Status.value_counts())/float(len(salaries)) * 100,2)
 
-# Subset data based on full time and part time.
-# Look at average salaries over time?
-# Unsupervised learning - new categories based on JobTitle
-# Toss any values out < 0?
+# Subset data based on Full Time (FT), Part Time (PT), or NaN
+salaries_FT = salaries.loc[salaries['Status'] == 'FT']
+salaries_PT = salaries.loc[salaries['Status'] == 'PT']
+salaries_NaN = salaries[(salaries['Status']!='FT') & (salaries['Status']!='PT')]
 
 # Convert most columns using convert_objects
 # df = df.convert_objects(convert_numeric=True)
+
+plt.scatter(salaries.Year, salaries.BasePay)
+plt.show()
